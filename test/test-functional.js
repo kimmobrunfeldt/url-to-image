a8168bf379736e82834da255c64f66a4cc2db358
Kimmo Brunfeldt <kimmo.brunfeldt@futurice.com>
2014-03-06 01:17:16 +0200

    Add couple of tests and travis config

---
+++ test/test-functional.js
@@ -0,0 +1,55 @@
+var assert = require("assert");
+var http = require('http');
+var fs = require("fs");
+var sizeOf = require('image-size');
+var UrlToImage = require('../');
+
+describe('UrlToImage', function() {
+
+    var server = http.createServer(function(req, res) {
+        res.end('<html>test</html>');
+    });
+
+    before(function(done) {
+        server.listen(9000);
+        server.on('listening', done);
+    });
+
+    after(function(done) {
+        server.close();
+        done();
+    });
+
+    describe('#render()', function() {
+        it('should render test image', function(done) {
+            UrlToImage.render('http://localhost:9000', 'localhost.png').done(function() {
+                var dimensions = sizeOf('localhost.png');
+                assert.equal(dimensions.width, 1280, 'default width is incorrect');
+                fs.unlinkSync('localhost.png');
+                done();
+            });
+        });
+
+        it('should render image in custom size', function(done) {
+            UrlToImage.render(
+                'http://localhost:9000',
+                'localhost.png', {
+                    width: 800,
+                    height: 600
+                }
+            ).done(function() {
+                var dimensions = sizeOf('localhost.png');
+
+                assert.equal(dimensions.width, 800, 'width is incorrect');
+
+                // The content of test page is so small, so viewport
+                // is larger than content. If content were larger,
+                // screenshot's height could be bigger than viewport's width
+                assert.equal(dimensions.height, 600, 'height is incorrect');
+
+                fs.unlinkSync('localhost.png');
+                done();
+            });
+        });
+    });
+});
