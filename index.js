123d89edb974f3c5ca9563f3a53f81e5ca0c0244
Kimmo Brunfeldt <kimmo.brunfeldt@futurice.com>
2014-03-06 00:29:59 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+var exec = require('child_process').exec;
+var _ = require('lodash');
+var Q = require('q');
+
+
+function UrlToImage() {
+    var api = {};
+
+    api.render = function(url, file, opts) {
+        var def = Q.defer();
+
+        var args = [
+            __dirname + 'url-to-image.js',
+            url,
+            file,
+            opts.width,
+            opts.height
+        ];
+
+        var execOpts = {
+            maxBuffer: Infinity
+        };
+
+        exec('phantomjs ' + args.join(' '), execOpts, function(err, stdout) {
+            def.resolve(err);
+        });
+
+        return def;
+    };
+
+    return api;
+}
+
+module.exports = UrlToImage;
